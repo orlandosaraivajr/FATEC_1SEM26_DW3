1bd324ecd0ffb8da758aaa2627ebbf92e6e6a1ae
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2026-02-23 21:46:47 -0300

    Oi mundo

---
+++ oimundo.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num1;
+    int num2;
+    int soma;
+    num1 = 10;
+    num2 = 15;
+    printf("Oi mundo\n");
+    printf("Oi Fatec\n");
+    printf("Oi Fatec Araras\n\n");
+    soma = num1 + num2;
+    printf("%i \n\n", soma);
+}
---
+++ oimundo2.py
@@ -0,0 +1,4 @@
+print(' Oi mundo 2')
+
+nome = input('Digite seu nome: ')
+print('Oi ' + nome)
